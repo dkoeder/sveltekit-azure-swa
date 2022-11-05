fdd9c89aa88acc6e1f5892fe787201d781bd8050
dkoeder <dkoeder@outlook.com>
2022-11-05 13:37:05 -0600

    sixth test commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit - fifth change</h1>
+<h1>Welcome to SvelteKit - sixth change</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
