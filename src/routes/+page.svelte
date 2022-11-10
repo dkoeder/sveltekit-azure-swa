a581afcdfbda82c5b857f0fa041920241f7c29c6
Darryl Koehn <dkoeder@outlook.com>
2022-11-09 21:10:38 -0700

    update page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit - Azure static web app</h1>
+<h1>Welcome to SvelteKit - Azure static web app - changed paths</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
