1ce35e3046ba6cee32ff2677eb792542fc0dbfdb
Darryl Koehn <dkoeder@outlook.com>
2022-11-12 09:58:47 -0700

    try to add an azure function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
+<script context="module">
+	export const prerender = true;
+
+	export async function load({ fetch }) {
+		const response = await fetch('/api/alive');
+
+		return {
+			status: response.status,
+			props: {
+				status: response.ok && (await response.json())
+			}
+		};
+	}
+</script>
+
+<script>
+	export let status;
+</script>
+
+<pre>{JSON.stringify(status, null, 2)}</pre>
 <h1>Welcome to SvelteKit - Azure static web app - changed paths</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<!-- for azure functions -->
+<!-- https://github.com/geoffrich/svelte-adapter-azure-swa -->
+ <!-- https://github.com/tguelcan/sveltekit-azure/blob/master/src/routes/index.svelte -->
